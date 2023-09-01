082fa550bfb0233d828ea04b48cd59a2392d94bf
Rodrigo Lopez <rodrigolp0521@gmail.com>
2023-09-01 00:00:17 -0600

    feat: add validation pipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { VersioningType, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import { config } from './config/swagger.config';
 
@@ -12,6 +12,13 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/v1/docs', app, document, {
     customSiteTitle: 'CommitList - API Docs',
